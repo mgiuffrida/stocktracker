7f1e1fedb0a99711b1f7dad3590d08d42277b448
Michael Giuffrida <michaelg@michaelg.us>
2017-10-10 18:42:50 -0700

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python2
 
-from setuptools import setup, find_packages
-
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+from setuptools import setup
 
 setup(
     name='stocktracker',
     version='0.0.1',
     description='stocktracker',
-    long_description=readme,
     author='Michael Giuffrida',
     author_email='michaelg@michaelg.us',
     url='https://github.com/mgiuffrida/stocktracker',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    license='MIT',
+    packages=['stocktracker']
 )
